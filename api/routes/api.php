7acf2aae3501b361dc2d1b59976844d7ef8bde93
KayDeeGee <kylebryanbasco@gmail.com>
2025-09-02 23:54:31 +0800

    WIP: saving local changes

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::apiResource('products', ProductController::class);
